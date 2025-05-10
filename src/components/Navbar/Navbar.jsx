7bd9b4280e080aae5c7c0a5dfb862f2ab67c0f00
Benedict Notoane <benni@MacBookPro.lan>
2025-05-10 20:29:09 +0200

    implement navbar structure and styles

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,11 +1,57 @@
-import styles from "./Navbar.module.css";
+import { faBars, faCartPlus, faUser } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faBars } from "@fortawesome/free-solid-svg-icons";
-import { faCartPlus } from "@fortawesome/free-solid-svg-icons";
-import { faUser } from "@fortawesome/free-solid-svg-icons";
+import { Link, NavLink } from "react-router-dom";
+import { getAuthContext } from "../../context/authContext";
+import Button from "../Button/Button";
+import styles from "./Navbar.module.css";
 
 const Navbar = () => {
-  return <nav></nav>;
+  const { user } = getAuthContext();
+  return (
+    <nav className={styles.navbar}>
+      {/* -------------------------- */}
+      <div className={styles.firstRow}>
+        <div className={styles.logo}>
+          <img
+            src="/icons/playstation-controller.svg"
+            alt="Playstation controller logo"
+          />
+        </div>
+        {/* -------------------------- */}
+        <div className={styles.cartHamburgerMenu}>
+          {user ? (
+            <Button className={styles.signOutButton}>Sign out</Button>
+          ) : (
+            <Link to="/sign-in" className={styles.SignInLink}>
+              Sign in
+            </Link>
+          )}
+          {user && (
+            <Link to="/profile" className={styles.profileButton}>
+              {user.imageUrl ? (
+                <img src={user.imageUrl} alt="User´s profile picture" />
+              ) : (
+                <FontAwesomeIcon icon={faUser} className={styles.profileIcon} />
+              )}
+            </Link>
+          )}
+          <Link className={styles.cartButton}>
+            <FontAwesomeIcon icon={faCartPlus} className={styles.cartIcon} />
+          </Link>
+
+          <Button className={styles.hamburgerButton}>
+            <FontAwesomeIcon icon={faBars} className={styles.hamburgerMenuIcon} />
+          </Button>
+        </div>
+      </div>
+      {/* -------------------------- */}
+      <div className={styles.secondRow}>
+        <NavLink to="/" className={({isActive})=>(isActive ? styles.activeLink : "")}>Home</NavLink>
+        <NavLink to="/games" className={({isActive})=>(isActive ? styles.activeLink : "")}>Games</NavLink>
+        <NavLink to="/contact" className={({isActive})=>(isActive ? styles.activeLink : "")}>Contact</NavLink>
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,14 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 
+import { RouterProvider } from "react-router-dom";
 import { AuthProvider } from "./context/authContext.jsx";
 import { router } from "./routes/routes.jsx";
 
 createRoot(document.getElementById("root")).render(
   // <StrictMode>
-  <AuthProvider router={router} />
+  <AuthProvider>
+    <RouterProvider router={router} />
+  </AuthProvider>
   // </StrictMode>,
 );
