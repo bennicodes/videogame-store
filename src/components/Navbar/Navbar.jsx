9921d69615b8d7220ab4be37136d8c33670c037c
Benedict Notoane <benni@MacBookPro.lan>
2025-05-10 20:35:54 +0200

    implement sign out feature

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,12 +1,27 @@
 import { faBars, faCartPlus, faUser } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { Link, NavLink } from "react-router-dom";
+import { signOut } from "firebase/auth";
+import { Link, Navigate, NavLink, useNavigate } from "react-router-dom";
+import { auth } from "../../../firebaseConfig";
 import { getAuthContext } from "../../context/authContext";
 import Button from "../Button/Button";
 import styles from "./Navbar.module.css";
 
 const Navbar = () => {
   const { user } = getAuthContext();
+  const navigate = useNavigate();
+
+  // Function to sign out the user
+  const handleSignout = async () => {
+    try {
+      await signOut(auth);
+      navigate("/");
+      console.log("User signed out");
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
   return (
     <nav className={styles.navbar}>
       {/* -------------------------- */}
@@ -20,9 +35,11 @@ const Navbar = () => {
         {/* -------------------------- */}
         <div className={styles.cartHamburgerMenu}>
           {user ? (
-            <Button className={styles.signOutButton}>Sign out</Button>
+            <Button className={styles.signOutButton} onClick={handleSignout}>
+              Sign out
+            </Button>
           ) : (
-            <Link to="/sign-in" className={styles.SignInLink}>
+            <Link to="/sign-in" className={styles.signInLink}>
               Sign in
             </Link>
           )}
@@ -40,15 +57,33 @@ const Navbar = () => {
           </Link>
 
           <Button className={styles.hamburgerButton}>
-            <FontAwesomeIcon icon={faBars} className={styles.hamburgerMenuIcon} />
+            <FontAwesomeIcon
+              icon={faBars}
+              className={styles.hamburgerMenuIcon}
+            />
           </Button>
         </div>
       </div>
       {/* -------------------------- */}
       <div className={styles.secondRow}>
-        <NavLink to="/" className={({isActive})=>(isActive ? styles.activeLink : "")}>Home</NavLink>
-        <NavLink to="/games" className={({isActive})=>(isActive ? styles.activeLink : "")}>Games</NavLink>
-        <NavLink to="/contact" className={({isActive})=>(isActive ? styles.activeLink : "")}>Contact</NavLink>
+        <NavLink
+          to="/"
+          className={({ isActive }) => (isActive ? styles.activeLink : "")}
+        >
+          Home
+        </NavLink>
+        <NavLink
+          to="/games"
+          className={({ isActive }) => (isActive ? styles.activeLink : "")}
+        >
+          Games
+        </NavLink>
+        <NavLink
+          to="/contact"
+          className={({ isActive }) => (isActive ? styles.activeLink : "")}
+        >
+          Contact
+        </NavLink>
       </div>
     </nav>
   );
